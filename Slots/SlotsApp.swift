659f79bfcdc2af853692431f47a8570ecfcc7a68
AngelikaKh <angelika1mos@gmail.com>
2021-05-16 19:50:32 +0400

    Initial Commit

---
+++ Slots/SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsApp.swift
+//  Slots
+//
+//  Created by Angelika Khodzhaian on 16.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
