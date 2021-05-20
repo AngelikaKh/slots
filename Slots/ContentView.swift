6d8df647e89a017e5d8947a6bd5cff0735baf4c9
AngelikaKh <angelika1mos@gmail.com>
2021-05-20 10:28:45 +0400

    slots. final part1

--- Slots/ContentView.swift
+++ Slots/ContentView.swift
@@ -8,9 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var credits = 1000
+    @State var slot1 = 1
+    @State var slot2 = 1
+    @State var slot3 = 1
+    
     var body: some View {
-        Text("Hello, world!")
+        
+        VStack(spacing: 10.0) {
+            Spacer()
+            Text("SwiftUI Slots").font(.largeTitle)
+            Spacer()
+            
+            Text("Credits: \(credits)")
+            
+            HStack {
+
+                Image("fruit\(slot1)")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                Image("fruit\(slot2)")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+                Image("fruit\(slot3)")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+            
+            Spacer()
+            Button("Spin") {
+                
+                slot1 = Int.random(in: 1...3)
+                slot2 = Int.random(in: 1...3)
+                slot3 = Int.random(in: 1...3)
+                
+                if slot1 == slot2 && slot2 == slot3 {
+                    credits += 200
+                } else {
+                    credits -= 50
+                }
+            }
             .padding()
+            .padding([.leading, .trailing], 40)
+            .foregroundColor(.white)
+            .background(Color(.systemPink))
+            .cornerRadius(25)
+            .font(.system(size: 18, weight: .bold, design: .default))
+            Spacer()
+        }
+        
     }
 }
 
